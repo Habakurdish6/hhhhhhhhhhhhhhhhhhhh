75e9990cc5fe8ee065a1f6c272c37de0824bc3cc
Habakurdish6 <76598323+Habakurdish6@users.noreply.github.com>
2021-01-27 20:06:13 +0300

    Update black.js

--- black.js
+++ black.js
@@ -21,7 +21,7 @@ var table = require("table").table;
 const Discord = require("discord.js");
 const cmd = require("node-cmd");
 const prefix = "%";
-client.login("Nzc5NjY5MTcxMjY0OTQ2MjE2.X7j5fw.YkWaV2lSiRa_-PdbWwHI1AmpWls");
+client.login("Nzc5NjY5MTcxMjY0OTQ2MjE2.X7j5fw.7O7EK3vCdFPFKjg9SMG7UsetNWs");
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user.username}!`);
   client.user.setStatus("idle");
